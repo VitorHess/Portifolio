e5c0a4348ea5e2223c2abb015b981dfa9d2fa1cf
Joao Vitor Hess Nardes <jvhn2006@gmail.com>
2024-07-05 01:04:09 -0300

    fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -7,3 +7,21 @@ window.addEventListener("scroll", function() {
     let header = document.querySelector('.header');
     header.classList.toggle('rolagem2', window.scrollY > 2600);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const target = document.getElementById(targetId);
+        if (target) {
+          const headerHeight = document.getElementById('header').offsetHeight; 
+          window.scrollTo({
+            top: target.offsetTop - headerHeight,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
